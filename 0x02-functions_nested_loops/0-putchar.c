13d20c1a37ae4256334bf43c959d31c807ecef65
Dararo <ednadararo@gmail.com>
2023-04-26 12:37:56 -0700

    changes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,7 @@
 #include "main.h"
-
-
 /**
- * main - prints _putchar in a new line
+ * main - Entry point
+ * Description: 'prints _putchar to stdout'
  * Return: Always 0
  */
 int main(void)
@@ -16,6 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
